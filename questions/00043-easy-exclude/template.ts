24b9a00fbbb12c8e13620a86b7e16066e90b16fc
Shuji Koike <shuji.koike@me.com>
2023-03-03 15:57:32 +0900

    wip

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,3 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = U extends T ? never : U
+
+type a = MyExclude<'a' | 'b' | 'c', 'a'>
--- questions/03057-easy-push/template.ts
+++ questions/03057-easy-push/template.ts
@@ -1,1 +1,6 @@
-type Push<T, U> = any
+type Push<T extends readonly any[], U> = {
+  [i in keyof T]: T[i]
+}
+
+type a = Push<[], 1>
+type b = Push<[1, 2], '3'>
