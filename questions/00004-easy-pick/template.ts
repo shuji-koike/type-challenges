5350d9ecca0bdd307f34892a8705e2fa5988624c
Shuji Koike <shuji.koike@me.com>
2023-03-03 15:05:36 +0900

    00004-easy-pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,6 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [k in K as k extends K ? k : never]: T[k]
+}
+
+// https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+// You can filter out keys by producing never via a conditional type:
